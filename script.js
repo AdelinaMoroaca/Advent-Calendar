927ff2448f64a729b706e5ea8646878b92f38958
Adelina Moroaca <moroacaadelina@gmail.com>
2023-12-14 22:57:44 +0200

    Add js  function that creates the rest of December  day's

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const date = new Date();
+const xmas = 25;
 const today = date.getDate();
 
 const dayBadge = document.querySelector('.header .badge');
 dayBadge.innerText = today;
+
+function createDays( limit = xmas){
+    const days = [];
+    for(let i = 1; i <= limit; i++){
+        days.push(i);
+    }
+    return days;
+}
+
+const days =createDays(xmas);
